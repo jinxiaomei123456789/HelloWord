3f1b5c1d9dc09f36d3bbdfb2cd2c5cf8381c6f27
jinxiaomei123456789 <3197299849@qq.com>
2017-08-04 15:34:02 +0800

    hello

---
+++ src/main/java/xiaomeiking/HelloWorld.java
@@ -0,0 +1,10 @@
+package xiaomeiking;
+
+/**
+ * Created by Administrator on 2017/8/4.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
